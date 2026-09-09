42f1e62548868ef74d9b9355b5fe92f787bfe8eb
ck1020974 <ck1020974@gmail.com>
2026-09-09 14:52:17 +0800

    hide outer horizontal overflow

--- site/app.js
+++ site/app.js
@@ -37,7 +37,7 @@
 const els = {};
 
 const fmt = new Intl.NumberFormat("zh-Hant-TW");
-const DATA_VERSION = "20260909-integrity-09";
+const DATA_VERSION = "20260909-integrity-10";
 
 const APPLY_SIEVE_SCORE_OVERRIDES = {
   "115-personal_application-008342-115_apply": {
